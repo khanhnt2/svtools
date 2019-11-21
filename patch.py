0818cde73f9fec861615493bf4ce58c8b91074f3
khanhnt2 <nguyentrungkhanh97@gmail.com>
2019-11-21 17:05:44 +0700

    refactor

--- patch.py
+++ patch.py
@@ -4,6 +4,40 @@ from lief import PE, ELF, parse
 import sys
 import struct
 from capstone import *
+from keystone import *
+
+
+class Shellcode():
+    def __init__(self, arch):
+        self.arch = arch
+        if arch == 'x86':
+            self.engine = Ks(KS_ARCH_X86, KS_MODE_32)
+            self._jmp = 'jmp %d'
+            self._save_state = 'pushf; push eax; push ebx; push ecx; push edx; push esi; push edi; push ebp'
+            self._restore_state = 'pop ebp; pop edi; pop esi; pop edx; pop ecx; pop ebx; pop eax; popf'
+        elif arch == 'x64':
+            self.engine = Ks(KS_ARCH_X86, KS_MODE_64)
+            self._jmp = 'jmp [rip]'
+            self._save_state = 'pushf; push rax; push rbx; push rcx; push rdx; push rsi; push rdi; push rbp; push r8; push r9; push r10; push r11; push r12; push r13; push r14; push r15'
+            self._restore_state = 'pop r15; pop r14; pop r13; pop r12; pop r11; pop r10; pop r9; pop r8; pop rbp; pop rdi; pop rsi; pop rdx; pop rcx; pop rbx; pop rax; popf'
+
+    def jmp(self, _from, _to):
+        shellcode = None
+        if self.arch == 'x86':
+            offset = _to - _from
+            shellcode, _ = self.engine.asm(self._jmp % offset)
+        elif self.arch == 'x64':
+            shellcode, _ = self.engine.asm(self._jmp)
+            shellcode += list(struct.pack('<Q', _to))
+        return shellcode
+
+    @property
+    def save_state(self):
+        return self.engine.asm(self._save_state)[0]
+
+    @property
+    def restore_state(self):
+        return self.engine.asm(self._restore_state)[0]
 
 
 def main():
@@ -12,10 +46,9 @@ def main():
     parser.add_argument('-s', '--shellcode', type=str, help='Shellcode file path', required=True)
     parser.add_argument('-e', '--entrypoint', action='store_false', help='Hook at entrypoint')
     parser.add_argument('-d', '--data', action='store_true', help='Create data section')
-    parser.add_argument('-a', '--address', type=lambda x: int(x, 0), help='Hook at address', default=0)
+    parser.add_argument('-a', '--address', type=lambda x: int(x, 0), nargs='*', help='Hook at address', default=[])
     t_arg = parser.add_argument('-o', '--output', type=str, help='Output file', default='patched')
 
-    # args = parser.parse_args(['..\\test\\python', '-s', 'F:\\tmp\\python_patched'])
     args = parser.parse_args()
 
     b = parse(args.file)
@@ -25,51 +58,43 @@ def main():
     section = create_section(b)
     code = add_section(b, section)
 
-    jmp_back = []
     architecture = get_cpu_architecture(b)
     if not architecture:
         print('Unknow architecture. Exit!')
         sys.exit(1)
 
-    if args.address != 0:  # hook at address
-        addr = args.address
-        cs_mode = None
-        if architecture == 'x86':
-            cs_mode = CS_MODE_32
-            jmplen = 5
-        else:
-            cs_mode = CS_MODE_64
-            jmplen = 14
+    if len(args.address) != 0 and len(args.address) == 1:  # hook at address
+        for addr in args.address:
+            cs_mode = None
+            if architecture == 'x86':
+                cs_mode = CS_MODE_32
+                jmplen = 5
+            else:
+                cs_mode = CS_MODE_64
+                jmplen = 14
 
-        # calculate number of bytes to patch
-        md = Cs(CS_ARCH_X86, cs_mode)
-        data = b.get_content_from_virtual_address(addr, 20)
-        ssum = 0
-        for ins in md.disasm(bytes(data), 0x1000):
-            ssum += ins.size
-            if ssum >= jmplen:
-                break
-        original_bytes = data[:ssum]
+            # calculate number of bytes to patch
+            md = Cs(CS_ARCH_X86, cs_mode)
+            data = b.get_content_from_virtual_address(addr, 20)
+            ssum = 0
+            for ins in md.disasm(bytes(data), 0x1000):
+                ssum += ins.size
+                if ssum >= jmplen:
+                    break
+            original_bytes = data[:ssum]
 
-        rva_addr = addr
-        jmp_to_addr = code.virtual_address
-        if is_pefile(b):
-            rva_addr -= b.optional_header.imagebase
-            jmp_to_addr = code.virtual_address + b.optional_header.imagebase
-        # jmp depend on architecture
-        if architecture == 'x86':
-            offset = code.virtual_address - rva_addr - 5
-            jmp_to = list(b'\xe9' + struct.pack('<I', offset & 0xffffffff))  # jmp offset
-            offset = rva_addr + ssum - (code.virtual_address + len(shellcode) + ssum) - 5
-            jmp_back = original_bytes + list(b'\xe9' + struct.pack('<I', offset & 0xffffffff))
-        else:
-            jmp_to = list(b'\xff\x25\x00\x00\x00\x00' + struct.pack('<Q', jmp_to_addr))  # jmp [RIP + 6]
-            jmp_back = original_bytes + list(b'\xff\x25\x00\x00\x00\x00' + struct.pack('<Q', addr + ssum))
-        jmp_to += [0x90] * (ssum - len(jmp_to))  # patch nop
-        b.patch_address(addr, jmp_to)
-        push_registers = [0x50, 0x53, 0x51, 0x52, 0x56, 0x57, 0x55, 0x41, 0x50, 0x41, 0x51, 0x41, 0x52, 0x41, 0x53, 0x41, 0x54, 0x41, 0x55, 0x41, 0x56, 0x41, 0x57]
-        pop_register = [0x41, 0x5F, 0x41, 0x5E, 0x41, 0x5D, 0x41, 0x5C, 0x41, 0x5B, 0x41, 0x5A, 0x41, 0x59, 0x41, 0x58, 0x5D, 0x5F, 0x5E, 0x5A, 0x59, 0x5B, 0x58]
-        shellcode = push_registers + shellcode + pop_register + jmp_back
+            section_addr = code.virtual_address
+            if is_pefile(b):
+                section_addr = code.virtual_address + b.optional_header.imagebase
+            # jmp depend on architecture
+            sc = Shellcode(architecture)
+            jmp_to = sc.jmp(addr, section_addr)
+            save_state = sc.save_state
+            restore_state = sc.restore_state
+            jmp_back = original_bytes + sc.jmp(section_addr + len(shellcode) + len(save_state) + len(restore_state) + ssum, addr + ssum)
+            jmp_to += [0x90] * (ssum - len(jmp_to))  # pad nop
+            b.patch_address(addr, jmp_to)
+            shellcode = save_state + shellcode + restore_state + jmp_back
     elif args.data:
         pass
     elif args.entrypoint:  # hook at entrypoint
@@ -83,14 +108,12 @@ def main():
         else:
             entrypoint = b.header.entrypoint
             b.header.entrypoint = code.virtual_address
-        if architecture == 'x86':
-            offset = entrypoint - (code.virtual_address + len(shellcode)) - 5
-            jmp_back = list(b'\xe9' + struct.pack('<I', offset & 0xffffffff))
-        else:
-            jmp_back = list(b'\xff\x25\x00\x00\x00\x00' + struct.pack('<Q', entrypoint))
-        push_registers = [0x50, 0x53, 0x51, 0x52, 0x56, 0x57, 0x55]
-        pop_register = [0x5D, 0x5F, 0x5E, 0x5A, 0x59, 0x5B, 0x58]
-        shellcode = push_registers + shellcode + jmp_back + pop_register
+
+        sc = Shellcode(architecture)
+        save_state = sc.save_state
+        restore_state = sc.restore_state
+        jmp_back = sc.jmp(code.virtual_address + len(shellcode) + len(save_state) + len(restore_state), entrypoint)
+        shellcode = save_state + shellcode + restore_state + jmp_back
 
     code.content = shellcode
     outfile = args.output if args.output != 'patched' else '%s_patched' % args.file
