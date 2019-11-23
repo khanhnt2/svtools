525745b5cc9cb43087f99b113b5e0b7841aea511
th <htht9673@gmail.com>
2019-11-23 10:13:40 +0000

    Update client.py

--- client.py
+++ client.py
@@ -2,7 +2,7 @@ import requests
 import socket
 
 HOST = '167.179.90.83'
-PORT = 8089
+PORT = 80
 URL = 'http://10.33.250.18:8080/submit_flag'
 COOKIE = ''
 HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0',
