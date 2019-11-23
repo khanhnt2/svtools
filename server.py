27571f2573a2acea19bbbf231ba7f723d941c808
khanhnt2 <nguyentrungkhanh97@gmail.com>
2019-11-23 16:39:46 +0700

    fix syntax error

--- server.py
+++ server.py
@@ -21,7 +21,7 @@ def recv(p):
 
 
 flag_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-flag_server.setsockopt(socket.SOL_SOCK, socket.SO_REUSEADDR, 1)
+flag_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 flag_server.bind('0.0.0.0', 8089)
 flag_server.listen(1)
 print('Wait client to connect...')
