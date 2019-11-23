d5b960d400088c5c8c93286c3d7b353688269acd
th <htht9673@gmail.com>
2019-11-23 08:04:16 +0000

    Update server.py

--- server.py
+++ server.py
@@ -1,16 +1,16 @@
 import socket, threading
 
-
 def decrypt(c):
-	t = b''
-	for d in c:
-		t += bytes([(d^1)])
-	return t
+        t = b''
+        for d in c:
+                t += bytes([(d^1)])
+        return t
 
 def recv(p):
-	l = p.recv(1024)
-	d = decrypt(l)
-	print("flag is",d)
+        l = p.recv(1024)
+        d = decrypt(l)
+        if b'UITCTF' in d:
+                print("flag is",d)
 
 s= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -19,12 +19,12 @@ s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind(('0.0.0.0',8080))
 s.listen(10)
 while 1:
-	try:
-		c, a = s.accept()
-		threading.Thread(target = recv(c))
-	except KeyboardInterrupt:
-		break
-	except Exception as e:
-		print(e)
-		pass
+        try:
+                c, a = s.accept()
+                threading.Thread(target = recv(c))
+        except KeyboardInterrupt:
+                break
+        except Exception as e:
+                print(e)
+                pass
 
