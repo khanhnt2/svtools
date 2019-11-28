2068ebf7c6fbd7658ece2bb30b58e3bc650c2b5f
khanhnt2 <nguyentrungkhanh97@gmail.com>
2019-11-28 20:06:26 +0700

    update logger

--- proxy/plugins/logger.py
+++ proxy/plugins/logger.py
@@ -1,7 +1,7 @@
 from base import PluginBase
+from graylog import Graylog
 import os
 import requests
-import base64
 import threading
 import logging
 
@@ -56,31 +56,21 @@ class LogFile(PluginBase):
         pass
 
 
-class GrayLog(PluginBase):
+class LogGraylog(PluginBase):
     '''Send to graylog server'''
     def __init__(self):
         self.enable = True
-
-    def thread_func(self, data, conn, who_send):
-        _data = str(data)
-        header = {'Content-Type': 'application/json'}
-        pdata = {"version": "1.1", "host": conn.app_name, 'short_message': _data, "level": 5, 'conn_id': conn.id, 'datetime': conn.datetime, 'who_send': who_send}
-        try:
-            requests.post('http://192.168.56.117:12201/gelf', json=pdata, headers=header, timeout=2.0)
-        except Exception as e:
-            logging.error('Send graylog: ' + str(e))
+        self.graylog = Graylog()
 
     def new_connection(self, conn):
         pass
 
     def send_client(self, data, conn):
-        t = threading.Thread(target=self.thread_func, args=(data, conn, 'server',))
-        t.start()
+        self.graylog.log_data(data, conn, 'server')
         return data
 
     def send_server(self, data, conn):
-        t = threading.Thread(target=self.thread_func, args=(data, conn, 'client',))
-        t.start()
+        self.graylog.log_data(data, conn, 'client')
         return data
 
     def finish_connection(self, conn):
@@ -107,19 +97,5 @@ class LogTcpStream(PluginBase):
         open(path, 'ab').write(data)
         return data
 
-    def thread_func(self, conn, who_send):
-        path = os.path.join(conn.app_name, str(conn.id) + '_tcpstream.log')
-        data = open(path, 'rb').read()
-        _data = str(data)
-        header = {'Content-Type': 'application/json'}
-        pdata = {"version": "1.1", "host": conn.app_name, "short_message": "1233", 'ahihi_message': _data, "level": 5, 'conn_id': conn.id, 'datetime': conn.datetime, 'who_send': who_send}
-        # pdata = {"version": "1.1", "host": conn.app_name, "short_message": "A short message", "level": 5, "_some_info": "foo"}
-        try:
-            requests.post('http://192.168.56.117:12201/gelf', json=pdata, headers=header, timeout=2.0)
-        except Exception as e:
-            logging.error('Send graylog: ' + str(e))
-
     def finish_connection(self, conn):
-        t = threading.Thread(target=self.thread_func, args=(conn, 'client',))
-        t.start()
         pass
